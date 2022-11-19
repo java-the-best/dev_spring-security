838943409188ae7d5ddbab93c5b11e034f1de422
viktorov_maxim <victorov_maxim@mail.ru>
2022-11-19 06:28:23 +0300

    springBoot

---
+++ src/main/java/localhost/dev_springsecurity/DevSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package localhost.dev_springsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevSpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevSpringSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/localhost/dev_springsecurity/DevSpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package localhost.dev_springsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevSpringSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
